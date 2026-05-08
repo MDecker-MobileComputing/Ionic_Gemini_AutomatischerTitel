087234d49abd43f7650fd44d7f488949c738b5b5
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-08 15:00:36 +0200

    Projektvorlage angepasst

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.eldecker.ionic.ueberschriftautomatisch',
   appName: 'Gemini_AutomatischeUeberschrift',
   webDir: 'www'
 };
