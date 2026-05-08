b608398c1dcec31ba5e97d522062d247c0e4397d
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-08 14:46:46 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Gemini_AutomatischeUeberschrift',
+  webDir: 'www'
+};
+
+export default config;
